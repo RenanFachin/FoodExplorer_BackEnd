0d250b1ef195ead22ff5b15a0705f3b79aec31ec
Renan <renan.fachin@hotmail.com>
2022-09-21 14:15:14 -0300

    :heavy_plus_sign: build: Instala sqlite

--- src/utils/AppError.js
+++ src/utils/AppError.js
@@ -12,3 +12,19 @@ class AppError {
 }
 
 module.exports = AppError;
+
+
+
+
+
+
+/* 
+Comentários sobre a criação a padronização de erros com esta classe
+
+message e statusCode são variáveis
+
+Constructor é um método que toda classe possui e este método é carregado automaticamente quando a classe é instanciada.
+O constructor irá receber 2 dados (message e statusCode), statusCode terá por default um valor pré definido como 400 para que não seja possível haver um erro sem código.
+
+O this dentro do constructor irá transferir o dado obtido durante a instanciação do objeto para as variáveis de escopo global
+*/
