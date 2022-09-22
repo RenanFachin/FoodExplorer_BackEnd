4cba8a8f0a375974b93caf601df17645eff06623
Renan <renan.fachin@hotmail.com>
2022-09-22 15:22:42 -0300

    :sparkles: feat: Cria migrations para automatizar criação de tabelas via js

--- src/server.js
+++ src/server.js
@@ -3,13 +3,16 @@ require('express-async-errors');
 const AppError = require('./utils/AppError')
 
 // Importando conexão com banco de dados relacional
-const database = require('./database/sqlite')
+const databaseMigrationsRun = require('./database/sqlite/migrations')
 
 // Importando bibliotecas
 const express = require("express");
 
 // Importando rotas (ele vai buscar o arquivo index.js)
 const routes = require('./routes');
+
+// Executando o banco de dados
+databaseMigrationsRun()
 
 // Inicializando biblioteca
 const app = express();
@@ -18,8 +21,6 @@ const app = express();
 app.use(express.json());
 app.use(routes)
 
-// Executando o banco de dados
-database()
 
 app.use((error, request, response, next) => {
     // Sabendo se é um error gerado pelo client
