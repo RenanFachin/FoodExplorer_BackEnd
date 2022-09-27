fbe17c2ee19d786ad6a03a575cb26b963c788a48
Renan <renan.fachin@hotmail.com>
2022-09-27 18:00:28 -0300

    :heavy_plus_sign: build: Install JsonWebToken as dependency

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -25,6 +25,9 @@ class SessionsController{
             throw new AppError ("E-mail e/ou senha incorreta", 401);
         }
 
+        // Gerando um Token de autenticação
+        
+
         return response.json(user);
     }
 };
